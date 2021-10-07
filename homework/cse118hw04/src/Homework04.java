b69b2170c82ed6a55549f8aa48251c3144556b16
Edwin Bermudes <74934197+redwinbee@users.noreply.github.com>
2021-10-07 09:29:36 -0400

    User a more accurate rep. of PI

--- homework/cse118hw04/src/Homework04.java
+++ homework/cse118hw04/src/Homework04.java
@@ -4,8 +4,7 @@ import java.util.Random;
 
 public class Homework04 {
     public static void main(String[] args) {
-        final double PI = 3.14159265;
-        double step1 = ((PI * (10_000)) + 0.5); // 31416.426500
+        double step1 = ((Math.PI * (10_000)) + 0.5); // 31416.426500
         double step2 = (int) step1; // 31416
         double rounded = step2 / 10_000.0; // 3.1416
 

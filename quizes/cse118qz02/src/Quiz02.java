791aad77557944c672b71e19614a5a8b316d3c1a
Edwin Bermudes <74934197+redwinbee@users.noreply.github.com>
2021-10-14 13:52:07 -0400

    qz02: complete

---
+++ quizes/cse118qz02/src/Quiz02.java
@@ -0,0 +1,55 @@
+package cse118qz02.src;
+
+import java.util.Random;
+
+public class Quiz02 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int n1 = rand.nextInt(21) + 10;
+        int n2 = rand.nextInt(21) + 10;
+        int n3 = rand.nextInt(21) + 10;
+        int n4 = rand.nextInt(21) + 10;
+
+        System.out.println(String.format("q01a: [n1=%d, n2=%d, n3=%d, n4=%d]", n1, n2, n3, n4));
+
+        double average = (n1 + n2 + n3 + n4) / 4.0;
+        System.out.println("q01b: average of n1..4 is " + average);
+
+        int evens = 0;
+        if (n1 % 2 == 0) {
+            evens++;
+        }
+        if (n2 % 2 == 0) {
+            evens++;
+        }
+        if (n3 % 2 == 0) {
+            evens++;
+        }
+        if (n4 % 2 == 0) {
+            evens++;
+        }
+
+        System.out.println("q01c: there are " + evens + " even numbers");
+        System.out.println("q01c: there are " + Math.abs(evens - 4) + " odd numbers");
+
+        // ------------------------------------------------
+
+        double var1 = rand.nextDouble(199.0) + 100.0;
+        System.out.println("q02a: var1 = " + var1);
+
+        int var2 = (int) var1;
+        System.out.println("q02b: var2 = " + var2);
+
+        double var3 = (int) ((var1 * 100) + 0.5); // ex: 123.456 -> 12346.000000
+        var3 /= 100.0;
+        System.out.println("q02c: var3 = " + var3);
+
+        // ------------------------------------------------
+
+        int length = rand.nextInt(101) + 10; // [10, 100]
+        int feet = length / 12;
+        int inches = Math.abs((feet * 12) - length);
+
+        System.out.println(String.format("q03: %din = [%d feet and %d inches]", length, feet, inches));
+    }
+}

54f907378b0422f8283b19858e553585e1f1cba9
Edwin Bermudes <74934197+redwinbee@users.noreply.github.com>
2021-10-26 17:25:55 -0400

    hw06: complete

--- homework/cse118hw06/src/Homework06.java
+++ homework/cse118hw06/src/Homework06.java
@@ -11,7 +11,7 @@ public class Homework06 {
         char letterLower = (char) (rand.nextInt(26) + 'a');
         System.out.println("[#1a] random lower-case letter: " + letterLower);
 
-        char digitChar = (char) (rand.nextInt(9) + '0');
+        char digitChar = (char) (rand.nextInt(10) + '0');
         System.out.println("[#1b] random digit char: " + digitChar);
 
         int n1 = rand.nextInt(36);
@@ -85,5 +85,84 @@ public class Homework06 {
         int lastIdx2 = str2.lastIndexOf(' ');
         String str2FW = str2.substring(0, idx2);
         String str2LW = str2.substring(lastIdx2 + 1);
+        System.out.printf("[#2h1] str1: firstWord={%s}, lastWord={%s}\n", str1FW, str1LW);
+        System.out.printf("[#2h1] str2: firstWord={%s}, lastWord={%s}\n", str2FW, str2LW);
+
+        // // --------------------------------------------------------------------
+
+        String helloJava = new String("Hello Java World");
+        char hJFirstLetter = helloJava.charAt(0);
+        char hjLastLetter = helloJava.charAt(helloJava.length() - 1);
+        String hJSubstr = helloJava.substring(1, helloJava.length() - 1);
+        System.out.println("[#3] original: " + helloJava);
+        System.out.println("[#3] modified: " + hjLastLetter + hJSubstr + hJFirstLetter);
+
+        // --------------------------------------------------------------------
+
+        System.out.print("[#4] type a word/nums (5 letters only): ");
+        String input = scan.next();
+
+        char i1 = input.charAt(0);
+        char i2 = input.charAt(input.length() - 1);
+        if (i1 == i2) {
+            i1 = input.charAt(1);
+            i2 = input.charAt(input.length() - 2);
+            if (i1 == i2) {
+                System.out.println("[#4] input is a palindrome");
+            } else {
+                System.out.println("[#4] input isn't a palindrome");
+            }
+        } else {
+            System.out.println("[#4] input isn't a palindrome");
+        }
+
+        // --------------------------------------------------------------------
+
+        int r1 = rand.nextInt(17);
+        char hx1 = 'A';
+        if (r1 < 8) {
+            hx1 = (char) (r1 + 'A');
+        } else {
+            hx1 = (char) ('0' + (r1 - 8));
+        }
+
+        int r2 = rand.nextInt(17);
+        char hx2 = 'A';
+        if (r2 < 8) {
+            hx2 = (char) (r2 + 'A');
+        } else {
+            hx2 = (char) ('0' + (r2 - 8));
+        }
+
+        int r3 = rand.nextInt(17);
+        char hx3 = 'A';
+        if (r3 < 8) {
+            hx3 = (char) (r3 + 'A');
+        } else {
+            hx3 = (char) ('0' + (r3 - 8));
+        }
+
+        int r4 = rand.nextInt(17);
+        char hx4 = 'A';
+        if (r4 < 8) {
+            hx4 = (char) (r4 + 'A');
+        } else {
+            hx4 = (char) ('0' + (r4 - 8));
+        }
+
+        System.out.println("[#5] random hex number: " + "0x" + hx1 + hx2 + hx3 + hx4);
+
+        // --------------------------------------------------------------------
+
+        final double METER_FEET = 3.28084;
+        double feet2M = 2 * METER_FEET;
+        double feet3M = 3 * METER_FEET;
+        double inch2M = feet2M * 12;
+        double inch3M = feet3M * 12;
+
+        System.out.printf("meter(s)  feet\t  inch(es)\n");
+        System.out.printf("  1 %10.2f\t%8.2f\n", METER_FEET, METER_FEET * 12);
+        System.out.printf("  2 %10.2f\t%8.2f\n", feet2M, inch2M);
+        System.out.printf("  3 %10.2f\t%8.2f\n", feet3M, inch3M);
     }
 }

95cb890f281e814264a06fba3ac63f7e1d140de1
Edwin Bermudes <74934197+redwinbee@users.noreply.github.com>
2021-11-04 13:06:03 -0400

    ex12: complete (kinda)

---
+++ exercises/cse118ex12/src/Exercise12.java
@@ -0,0 +1,99 @@
+package cse118ex12.src;
+
+public class Exercise12 {
+    public static void main(String[] args) {
+        // all parts of the for loop are optional
+        int i;
+        for (i = 0; i < 5; i++) {
+            System.out.println("Welcome to Java!");
+        }
+
+        // sum the numbers from 1..n
+        int n = (int) (Math.random() * 100);
+        int sum;
+        for (i = 1, sum = 0; i <= n; i++) {
+            sum += i;
+        }
+        System.out.printf("summation of 1..%d is = %d\n", n, sum);
+
+        /**
+         * create a solution to the solve the problem of predicting tuition at a given
+         * school where the tuition increases 7% ever year starting at $10,000. In how
+         * many years will the tuition have doubled?
+         */
+        double tuition = 10_000;
+        int year;
+        for (year = 0; (tuition <= 20_000); year++) {
+            tuition *= 1.07;
+            System.out.printf("[year %d] tuition: %f\n", year, tuition);
+        }
+        System.out.println("the tuition will have doubled by year " + year);
+
+        /**
+         * find the greatest common divisor for two given numbers. Note that this
+         * solution isn't very efficient.
+         */
+        int j = (int) (Math.random() * 100);
+        int k = (int) (Math.random() * 100);
+        int gcd = 1;
+        for (i = 2; i <= Math.min(j, k); i++) {
+            if (j % i == 0 && k % i == 0) {
+                gcd = i;
+            }
+        }
+        System.out.printf("gcd of (%d, %d) is %d\n", j, k, gcd);
+
+        /** print all the numbers from 1..rn and add a new line every 10 numbers */
+        int rn = (int) (Math.random() * 100);
+        for (i = 0; i <= rn; i++) {
+            if (i % 10 == 0) {
+                System.out.println();
+            }
+            System.out.printf("%d\t", i);
+        }
+
+        rn = (int) (Math.random() * 100);
+        int next = 0;
+        for (i = 0; i <= rn; i++) {
+            if (i % 10 == 0) {
+                System.out.println();
+            }
+
+            next = (int) (Math.random() * 100);
+            System.out.printf("[%d] %d\t", i, next);
+        }
+
+        rn = (int) (Math.random() * 100); // [0, 99]
+        /**
+         * min and max are set to upper-bound+1 and lower-bound-1, respectively to
+         * ensure the min and max are updated at least 1 time, otherwise it's possible
+         * the numbers will never be updated and the values will be incorrect
+         * 
+         * 
+         * FIX
+         */
+        // int min = 100;
+        // int max = -1;
+        // rn = (int) (Math.random() * 100);
+        // for (i = 0; i <= rn; i++) {
+        // if (i % 10 == 0) {
+        // System.out.println();
+        // }
+        // next = (int) (Math.random() * 100);
+        // System.out.printf("[%d] %d\t", i, next);
+
+        // if (rn < min) {
+        // min = rn;
+        // }
+        // if (rn > max) {
+        // max = rn;
+        // }
+
+        // sum += rn;
+        // }
+        // System.out.println();
+        // System.out.println("min: " + min);
+        // System.out.println("max: " + max);
+        // System.out.println("avg: " + sum /;
+    }
+}

c7d73b974631d077c72e6a9165c0b8ee988931e3
Edwin Bermudes <74934197+redwinbee@users.noreply.github.com>
2021-11-04 16:17:33 -0400

    ex12: implement min/max checking

--- exercises/cse118ex12/src/Exercise12.java
+++ exercises/cse118ex12/src/Exercise12.java
@@ -51,49 +51,39 @@ public class Exercise12 {
             }
             System.out.printf("%d\t", i);
         }
+        System.out.println();
+        System.out.println();
 
+        /**
+         * generate numbers from 1..rn and generate a random number for rn-many times
+         * then calculate the min and max of those numbers. All while following the same
+         * spacing as before; newline after every 10 numbers.
+         * 
+         * note: the min initial value was set to the (upper-bounds + 1) and the max
+         * value was set to the (lower-bounds - 1), this way we ensure that both
+         * variables are GUARANTEED to be updated at least 1 time because there is a
+         * non-zero chance the inital value we choose for the variables won't allow the
+         * if statements to update their values
+         */
         rn = (int) (Math.random() * 100);
-        int next = 0;
+        int min = 100;
+        int max = -1;
         for (i = 0; i <= rn; i++) {
             if (i % 10 == 0) {
                 System.out.println();
             }
+            int next = (int) (Math.random() * 100);
+            System.out.printf("%d\t", next);
 
-            next = (int) (Math.random() * 100);
-            System.out.printf("[%d] %d\t", i, next);
+            if (next < min) {
+                min = next;
+            }
+            if (next > max) {
+                max = next;
+            }
         }
-
-        rn = (int) (Math.random() * 100); // [0, 99]
-        /**
-         * min and max are set to upper-bound+1 and lower-bound-1, respectively to
-         * ensure the min and max are updated at least 1 time, otherwise it's possible
-         * the numbers will never be updated and the values will be incorrect
-         * 
-         * 
-         * FIX
-         */
-        // int min = 100;
-        // int max = -1;
-        // rn = (int) (Math.random() * 100);
-        // for (i = 0; i <= rn; i++) {
-        // if (i % 10 == 0) {
-        // System.out.println();
-        // }
-        // next = (int) (Math.random() * 100);
-        // System.out.printf("[%d] %d\t", i, next);
-
-        // if (rn < min) {
-        // min = rn;
-        // }
-        // if (rn > max) {
-        // max = rn;
-        // }
-
-        // sum += rn;
-        // }
-        // System.out.println();
-        // System.out.println("min: " + min);
-        // System.out.println("max: " + max);
-        // System.out.println("avg: " + sum /;
+        System.out.println();
+        System.out.println("min: " + min);
+        System.out.println("max: " + max);
     }
 }

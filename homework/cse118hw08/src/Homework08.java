ef2babe3087b6eb2b2d8a566c19ec965645e2dae
Edwin Bermudes <74934197+redwinbee@users.noreply.github.com>
2021-11-16 11:17:42 -0500

    hw08: more work

--- homework/cse118hw08/src/Homework08.java
+++ homework/cse118hw08/src/Homework08.java
@@ -32,5 +32,59 @@ public class Homework08 {
 
         // -------------------------------------------------------------
 
+        System.out.print("[q2] type a string: ");
+        String input = scan.nextLine();
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i); // seems inefficient
+        }
+        System.out.println("[q2] original string: " + input);
+        System.out.println("[q2] reversed string: " + reversed);
+
+        // -------------------------------------------------------------
+
+        int rn = rand.nextInt(100_000) + 1;
+        int num = rn;
+        int rev = 0;
+        while (num > 0) {
+            int rem = num % 10;
+            rev = rev * 10 + rem;
+            num /= 10;
+        }
+        System.out.println("[q3a] original num: " + rn);
+        System.out.println("[q3b] reversed num: " + rev);
+
+        // -------------------------------------------------------------
+
+        int lot1 = rand.nextInt(9) + 1;
+        int lot2 = 0;
+        do {
+            lot2 = rand.nextInt(10) + 1;
+        } while (lot1 == lot2);
+        System.out.println("[q4] lotter number: " + lot1 + lot2);
+
+        // -------------------------------------------------------------
+
+        System.out.print("[q5] enter a positive number: ");
+        int n = Integer.parseInt(scan.nextLine());
+        double summation = 1;
+        for (double i = 2.0; i <= n; i++) {
+            if (i % 2 <= 0.000000) {
+                summation -= (1 / i);
+            } else {
+                summation += (1 / i);
+            }
+        }
+        System.out.printf("[q5] sum(1 +/- (1 / n)), n = %s is: %f\n", n, summation);
+
+        // -------------------------------------------------------------
+
+        // sequence: 1, 1, 2, 3, 5, 8, 13, 21, ...
+        /*
+         * pattern: starting at 1, add the first number to the previous (1 + 0) and
+         * continue with the result -> (1 + 2) = 3 , (2 + 3) = 5, (3 + 5) = 8, ...
+         */
+
+        System.out.print("1, ");
     }
 }

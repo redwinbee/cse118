82c73edecf2f81bd88b50d3bcaf12195955efc16
Edwin Bermudes <74934197+redwinbee@users.noreply.github.com>
2021-12-05 14:50:15 -0500

    hw10: break as soon as we know

--- homework/cse118hw10/src/Homework10.java
+++ homework/cse118hw10/src/Homework10.java
@@ -134,6 +134,7 @@ public class Homework10 {
         for (int i = 0; i < array.length - 1; i++) {
             if (array[i] > array[i + 1]) {
                 isAscending = false;
+                break;
             }
         }
         System.out.println();
